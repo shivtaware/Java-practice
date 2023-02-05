9f56936d4e61a317935a44e43498266eb8726002
Dell <Dell@DESKTOP-2T20N3O>
2023-02-05 23:09:29 +0530

    Collection practice added

---
+++ src/CollectionPractice.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CollectionPractice {
+
+	public static void main(String[] args) {
+		
+		Collection nums= new ArrayList();
+		
+		
+		
+		nums.add(1);
+		nums.add(2);
+		nums.add(3);
+		nums.add(4);
+		
+//		System.out.println(nums);
+		
+		Iterator it= nums.iterator();
+		  
+		while(it.hasNext())
+		  System.out.println(it.next());
+		 
+		
+
+	}
+
+}
